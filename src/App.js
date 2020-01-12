e07f543275dad827c79e1bcc763bcdeb081c5506
Abhishek Kumar <abhishekbhaskar35@gmail.com>
2020-01-13 02:16:11 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import NavBar from './components/NavBar';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/rooms/">
+          <Rooms />
+        </Route>
+        <Route exact path="/rooms/:slig">
+          <SingleRoom />
+        </Route>
+        <Route>
+          <Error></Error>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: 'Free cocktails',
+        info:
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ut.'
+      },
+      {
+        icon: <FaHiking />,
+        title: 'Endless Hiking',
+        info:
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ut.'
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: 'Free Shuttle',
+        info:
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ut.'
+      },
+      {
+        icon: <FaBeer />,
+        title: 'Strongest Beer',
+        info:
+          ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ut.'
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="services">
+        <Title title="Services" />
+        <div className="service-center">
+          {this.state.services.map((item, i) => {
+            return (
+              <article key={i} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
